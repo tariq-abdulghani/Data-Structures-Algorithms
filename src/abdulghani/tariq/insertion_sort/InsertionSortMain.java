1bcd83ea2771f8ec98a1d0aaec8af227d4e6be17
Tariq Abdulghani <tariq.abdulghani.hussein@gmail.com>
2021-04-01 09:36:32 +0200

    impl insertion sort.

--- src/abdulghani/tariq/insertion_sort/InsertionSortMain.java
+++ src/abdulghani/tariq/insertion_sort/InsertionSortMain.java
@@ -6,7 +6,7 @@ import abdulghani.tariq.models.Order;
 public class InsertionSortMain {
     public static void main(String[] args) {
 
-        int[] intArray = {9, 1,4,50, 2, 45, -9};
+        int[] intArray = {9, 1,4,-100,50, 20, 1000, 45, -9, -8, -7};
         intArray = new InsertionSort().sort(intArray, Order.ASC);
 
         for (int i: intArray
