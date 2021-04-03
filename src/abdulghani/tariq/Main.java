e3a2647836ad1f185f4d7dd1260ae0968344edfc
Tariq Abdulghnai <tariq.abdulghani.hussein@gmail.com>
2021-04-03 15:15:27 +0200

    testing our knowledge with isertion and shell sort algos in main

--- src/abdulghani/tariq/Main.java
+++ src/abdulghani/tariq/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         // write your code here
 
-        int[] ints = {10, 1, 4, 8, 90, 15, 35, 7, -7, 20, -8, 18, 9};
+        int[] ints = {10, 1, 4, 8, 90, 15, 35, 7, -7, 20, -8, 18, 9, 0};
         int gap = 3;
 //        for (int i = gap; i < ints.length; i++) {
 //            int temp = ints[i];
@@ -18,17 +18,33 @@ public class Main {
 //    }
 
         // increase sorted partition by one
-        for(int i = gap; i< ints.length; i ++){
-            int temp = ints[i];
-            int j;
-            // rearrange sorted partition
-            // by looking for insertion point of temp
+//        for(int i = gap; i< ints.length; i ++){
+//            int temp = ints[i];
+//            int j;
+//            // rearrange sorted partition
+//            // by looking for insertion point of temp
+//
+//            for (j = i; j>= gap && ints[j-gap] > temp; j = j-gap) {
+//                ints[j] = ints[j - gap];
+//            }
+//            ints[j] = temp;
+//        }
 
-            for (j = i; j>= gap && ints[j-gap] > temp; j = j-gap) {
-                ints[j] = ints[j - gap];
+
+        // sell sort
+
+        // traversing unsorted part
+        for(int unsortedIndex=1; unsortedIndex < ints.length; unsortedIndex++){
+            int temp = ints[unsortedIndex];
+            int j;
+            // finding insertion point loop
+            for(j = unsortedIndex; j> 0 && ints[j-1] /*shift only if j-1 element is less*/> temp; j --){
+                ints[j] = ints[j - 1]; // shift by one to the right to make space for the inserted element.
             }
+            // out side the loop we found insertion point?
             ints[j] = temp;
         }
+
         for(int i: ints){
             System.out.println(i);
         }
